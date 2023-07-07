5b891e41a8d7b890effa90b9be72aea053104862
PawelBarbarski <barbarski.pawel@gmail.com>
2023-07-08 00:16:32 +0200

    Add empty Kotlin project

---
+++ buildSrc/src/main/kotlin/ordo.kotlin-common-conventions.gradle.kts
@@ -0,0 +1,21 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
